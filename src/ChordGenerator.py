4c585c2bdfe3f086e9aeb8bccc706a779cb5af00
DeclanGazil02 <declan.gazil@ufl.edu>
2024-09-22 13:47:55 -0400

    added strum notes to be returned as well along side chord

--- src/ChordGenerator.py
+++ src/ChordGenerator.py
@@ -1,4 +1,11 @@
 import audiogen
+import pyaudio 
+
+import threading
+import time
+import itertools
+
+import math
 
 class ChordGenerator: # passes back chord with tonality and extension(s) based on button
 
@@ -37,24 +44,58 @@ class ChordGenerator: # passes back chord with tonality and extension(s) based o
 
         elif self.tonality == "m": # build out minor chord
             chord = self.buildChord(midiVal, self.minorChord)
-
-        return chord
+        
+        strumNotes = self.getStrumPadNotes()
+        return (chord, strumNotes)
     
     def getChordHertz(self): 
-        midiChord = self.getChordMidi()
+        midiChord, strumNotes = self.getChordMidi() # convert midi to freq
         freqChord = []
+        freqStrum = []
+
         for val in midiChord:
             freqChord.append(self.convertMidiToFreq(val))
-            
-        return freqChord
+
+        for val in strumNotes:
+            freqStrum.append(self.convertMidiToFreq(val))
+
+        return (freqChord, freqStrum)
 
     def convertMidiToFreq(self, midiVal):
-        return 440 * (2 ** ((midiVal - 69)/12))
+        return math.floor(440 * (2 ** ((midiVal - 69)/12))) #TODO: DISCUSS FLOOR VS CEIL
+
+    def getStrumPadNotes(self):
+        midiVal = self.circleOfFifths.get(self.root)
+        strumNotes = []
+        additive = 0
+        octave = 12
+        for i in range(8): # to be discussed how many notes to have
+            strumNotes.append(midiVal + additive + octave)
+            additive = additive + 3 # build up in thirds
+        
+        return strumNotes
 
 
-cG = ChordGenerator("Db", "m", ["7"])
+
+cG = ChordGenerator("Db", "M", ["7", "9"])
 print(cG.getChordMidi())
 print(cG.getChordHertz())
 
-audiogen.sampler.play(audiogen.tone(440))
-audiogen.crop
+# p = pyaudio.PyAudio() # instantiate pyAudio or else it does not work
+
+# def playNoteThread(freq):
+#     audiogen.sampler.play(audiogen.tone(frequency=freq))
+
+
+# def playChord(chord):
+#     listOfThreads = []
+
+#     for note in chord:
+#         print("starting thread for", note)
+#         thread = threading.Thread(target=playNoteThread, args=(note,))
+#         listOfThreads.append(thread)
+#         thread.start()
+#     return listOfThreads
+
+# playChord(cG.getChordHertz())
+
