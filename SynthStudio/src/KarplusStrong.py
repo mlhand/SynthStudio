61061b925cdfecc35f6b9aa2dd8086188229ea80
Aidan Crawford <crawforda@ufl.edu>
2024-10-13 23:21:58 +0200

    Create Karplus Strong cycle implementation

--- SynthStudio/src/KarplusStrong.py
+++ SynthStudio/src/KarplusStrong.py
@@ -2,6 +2,7 @@ import math
 import random
 
 def createWavetable(sample_count):
+    # To create a sound with a frequency of F, set the sample_count to (sample_rate/F) - 1/2
     high = 1.0
     low = -1.0
     wavetable = []
@@ -10,4 +11,18 @@ def createWavetable(sample_count):
             wavetable.append(high)
         else:
             wavetable.append(low)
+    return wavetable
+    
+def karplusStrongCycle(wavetable, soundlength):
+    tablelength = len(wavetable)
+    currentval = 0
+    lastval = 0
+    fullwavetable = []
+    for i = 0; i < soundlength; i++:
+        wavetable[currentval] = wavetable[currentval]/2 + wavetable[lastval]/2
+        fullwavetable.append(wavetable[currental])
+        lastval = currentval
+        currentval = currentval + 1
+        if currentval >= tablelength:
+            currentval = 0
     return wavetable
