be953d824232443f5c5ce8b7beddd49ab4bb197d
Devon S <me@dstet.me>
2024-10-16 17:24:45 +0100

    Added initial setup script

---
+++ init-env.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+FIRMWARE_CONFIG="/boot/firmware/config.txt"
+CMDLINE="/boot/firmware/cmdline.txt"
+MODULES="/etc/modules"
+HOMEDIR=$(eval echo ~$USER)
+BASHRC="~/.bashrc"
+
+# Add necessary contents to bashrc
+cat >>$BASHRC << EOL
+
+export PATH="$HOMEDIR/.local/bin:\$PATH"
+export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
+EOL
+
+# Install poetry
+curl -sSL https://install.python-poetry.org | python3 -
+
+# Modify configuration files
+cat >>$FIRMWARE_CONFIG << EOL
+[all]
+dtoverlay=dwc2 
+EOL
+
+sed -i -e 's/otg_mode=1/#otg_mode=1/g' $FIRMWARE_CONFIG
+
+sed -i 's/$/ modules-load=dwc2,g_midi/' $CMDLINE
+
+cat >>$MODULES << EOL 
+i2c-dev
+dwc2
+g_midi
+EOL
+
+echo "Reboot to use the device!"
