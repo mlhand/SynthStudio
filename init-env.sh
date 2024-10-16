bf807192e4d3bb393460ac74d1e6724e5b2155cc
Devon <devon.stettler@tutanota.com>
2024-10-16 14:02:33 -0400

    Fix permissions error with poetry installation

--- init-env.sh
+++ init-env.sh
@@ -3,20 +3,25 @@
 FIRMWARE_CONFIG="/boot/firmware/config.txt"
 CMDLINE="/boot/firmware/cmdline.txt"
 MODULES="/etc/modules"
-HOMEDIR=$(eval echo ~$USER)
+HOMEDIR=$(eval echo ~$SUDO_USER)
 BASHRC="$HOMEDIR/.bashrc"
 
 # Add necessary contents to bashrc
+echo "Modifying $BASHRC"
 cat >>$BASHRC << EOL
 
 export PATH="$HOMEDIR/.local/bin:\$PATH"
 export PYTHON_KEYRING_BACKEND=keyring.backends.fail.Keyring
 EOL
 
-# Install poetry
-curl -sSL https://install.python-poetry.org | python3 -
+# Install poetry as current user, not sudo
+echo "Installing poetry as $SUDO_USER"
+su $SUDO_USER <<'EOF'
+curl -sSL https://install.python-poetry.org | python3 - && echo $USER
+EOF
 
 # Modify configuration files
+echo "Modifying $FIRMWARE_CONFIG"
 cat >>$FIRMWARE_CONFIG << EOL
 [all]
 dtoverlay=dwc2 
@@ -24,14 +29,19 @@ EOL
 
 sed -i -e 's/otg_mode=1/#otg_mode=1/g' $FIRMWARE_CONFIG
 
+echo "Modifying $CMDLINE"
 sed -i 's/$/ modules-load=dwc2,g_midi/' $CMDLINE
 
+echo "Modifying $MODULES"
 cat >>$MODULES << EOL 
 dwc2
 g_midi
 EOL
 
-source $BASHRC
+echo "Sourcing $BASHRC"
+. $BASHRC
+
+echo "Configuring poetry environment for SynthStudio"
 
 poetry config virtualenvs.in-project true
 
