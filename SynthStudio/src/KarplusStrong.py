2278eeefbff2e85279a8f2bc95d84fd5997919f6
mlhand <matthand10@gmail.com>
2024-10-30 20:22:32 -0400

    Keyboard with karplus strong, added new wavetables

--- SynthStudio/src/KarplusStrong.py
+++ SynthStudio/src/KarplusStrong.py
@@ -15,6 +15,42 @@ def createWavetable(sample_count):
         else:
             wavetable.append(low)
     return wavetable
+
+def createWavetableSquare(sample_count):
+    # To create a sound with a frequency of F, set the sample_count to (sample_rate/F) - 1/2
+    # The default PyGame sample rate is 44100Hz
+    high = 1.0
+    low = -1.0
+    wavetable = []
+    highBool = True
+    squareLength = 50     # arbitrary
+    counter = 0
+    for i in range(0, sample_count):
+        
+        if highBool:
+            wavetable.append(high)
+        else:
+            wavetable.append(low)
+        counter += 1
+        if counter == squareLength:
+            highBool = not highBool
+            counter = 0
+    return wavetable
+
+def createWavetableTriangle(sample_count):
+    high = 1.0
+    low = -1.0
+    wavetable = []
+    highBool = True
+    squareLength = 50     # arbitrary
+    counter = 0
+    section = sample_count // 4
+    for i in range(0, 2):
+        for j in range(section):
+            wavetable.append(j*(1/section) * high)
+        for k in range(section):
+            wavetable.append(k*(1/4)*low)
+    return wavetable
     
 def karplusStrongCycle(wavetable, soundlength):
     tablelength = len(wavetable)
@@ -36,10 +72,10 @@ def createSound(frequency, length = 6):
     buffer = np.array(karplusStrongCycle(wavetable, freqlength)).astype(np.float32)
     sound = pygame.mixer.Sound(buffer)
     sound.play(0)
-    # pygame.time.wait(int(sound.get_length() * 1000)) # Is this necessary?
+    #pygame.time.wait(int(sound.get_length() * 1000)) # Is this necessary?
     
     
 
 pygame.mixer.init(size=32, channels=1)
 
-createSound(440)
+#createSound(440)
---
+++ SynthStudio/src/KeyboardKarplus.py
@@ -0,0 +1,129 @@
+import threading
+import itertools
+import math
+import pygame.midi
+import time
+import keyboard
+from ChordGenerator import ChordGenerator
+import KarplusStrong as kp
+
+device = 0     # device number in win10 laptop
+instrument = 24 # http://www.ccarh.org/courses/253/handout/gminstruments/
+volume = 127
+wait_time = .05
+pygame.midi.init()
+player = pygame.midi.Output(device)
+player.set_instrument(instrument)
+
+#global quitProgram, minor, currentRoot, seventh, ninth, currentChord
+
+quitProgram = False
+currentRoot = "C"
+minor = False
+seventh = False
+ninth = False
+currentChord = ChordGenerator("C", "M", [])
+notesPlaying = []
+octave = -1 # does nothing right now
+
+def setCurrentRoot(newRoot):
+    global currentRoot
+    currentRoot = newRoot
+    setCurrentChord()
+    playChord()
+
+def playChord():
+    for note in notesPlaying: 
+        player.note_off(note, volume)
+
+    chord = currentChord.getChordAndStrumPadHertz()[0]
+    for note in chord:
+        kp.createSound(note, 2)
+        notesPlaying.append(note)
+
+def setCurrentChord():
+    global currentChord
+    tonality = "M"
+    if (minor == True):
+        tonality = "m"
+    if (not seventh and not ninth):
+        currentChord = ChordGenerator(currentRoot, tonality, [])
+    elif (seventh and not ninth):
+        currentChord = ChordGenerator(currentRoot, tonality, ["7"])
+    elif (not seventh and ninth):
+        currentChord = ChordGenerator(currentRoot, tonality, ["9"])
+    else:
+        currentChord = ChordGenerator(currentRoot, tonality, ["7", "9"])
+
+def playNote(noteNum):
+    chord = currentChord.getChordAndStrumPadHertz()[0] + currentChord.getChordAndStrumPadHertz()[1]
+    note = chord[noteNum]
+    kp.createSound(note, 2)
+
+def setMinor():
+    global minor
+    minor = not minor
+    setCurrentChord()
+
+def setSeventh():
+    global seventh
+    seventh = not seventh
+    setCurrentChord()
+
+def setNinth():
+    global ninth
+    ninth = not ninth
+    setCurrentChord()
+
+def changeInstrument(num):
+    global instrument
+    instrument += num
+    player.set_instrument(instrument)
+
+def exitProgram():
+    quitProgram = True
+    
+#Functionality Buttons
+keyboard.on_press_key('esc', lambda x: exitProgram())
+keyboard.on_press_key('=', lambda x: changeInstrument(1))
+keyboard.on_press_key('-', lambda x: changeInstrument(-1))
+
+#Chord Selection Buttons
+keyboard.on_press_key('q', lambda x: setCurrentRoot("Db"))
+keyboard.on_press_key('w', lambda x: setCurrentRoot("Ab"))
+keyboard.on_press_key('e', lambda x: setCurrentRoot("Eb"))
+keyboard.on_press_key('r', lambda x: setCurrentRoot("Bb"))
+keyboard.on_press_key('a', lambda x: setCurrentRoot("F"))
+keyboard.on_press_key('s', lambda x: setCurrentRoot("C"))
+keyboard.on_press_key('d', lambda x: setCurrentRoot("G"))
+keyboard.on_press_key('f', lambda x: setCurrentRoot("D"))
+keyboard.on_press_key('z', lambda x: setCurrentRoot("A"))
+keyboard.on_press_key('x', lambda x: setCurrentRoot("E"))
+keyboard.on_press_key('c', lambda x: setCurrentRoot("B"))
+keyboard.on_press_key('v', lambda x: setCurrentRoot("F#"))
+
+#Tonalities
+keyboard.on_press_key('m', lambda x: setMinor())
+keyboard.on_press_key('k', lambda x: setSeventh())
+keyboard.on_press_key('l', lambda x: setNinth())
+
+#Individual Notes
+keyboard.on_press_key('1', lambda x: playNote(0))
+keyboard.on_press_key('2', lambda x: playNote(1))
+keyboard.on_press_key('3', lambda x: playNote(2))
+keyboard.on_press_key('4', lambda x: playNote(3))
+keyboard.on_press_key('5', lambda x: playNote(4))
+keyboard.on_press_key('6', lambda x: playNote(5))
+keyboard.on_press_key('7', lambda x: playNote(6))
+keyboard.on_press_key('8', lambda x: playNote(7))
+
+
+
+while not quitProgram:
+    keyboard.wait()
+#time.sleep(1000000)
+
+
+# close the device -------------------------------------------------------------
+del player
+pygame.midi.quit()
